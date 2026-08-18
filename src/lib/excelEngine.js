22d82067b8f677753fb8e9ba9517407f4679edad
Revanth015 <115963624+Revanth015@users.noreply.github.com>
2026-08-18 16:18:55 +0530

    feat: expose shared Excel intelligence engine

---
+++ src/lib/excelEngine.js
@@ -0,0 +1,5 @@
+export { readExcelFile } from "../intelligence/evaluator/fileReader";
+export { analyzeWorkbook } from "../intelligence/evaluator/evidenceEngine";
+export { understandMission } from "../intelligence/evaluator/missionUnderstanding";
+export { evaluateCriteria } from "../intelligence/evaluator/criterionEvaluator";
+export { calculateFinalScore, scoreToGrade } from "../intelligence/evaluator/scoringEngine";
